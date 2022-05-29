b99158253740549d5af72584e991091d310d0d23
JakaV1 <106039789+JakaV1@users.noreply.github.com>
2022-05-29 18:43:23 +0700

    Update and rename plugins/creator.js to plugin/creator.js

--- plugin/creator.js
+++ plugin/creator.js
@@ -20,14 +20,14 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *Nama RP:* Ndaaa
+${htjava} *Nama RP:* Jaka
 ${htjava} *Nama RL:* private
-${htjava} *Umur:* private
-${htjava} *Kelas:* private
+${htjava} *Umur:* 16🗿
+${htjava} *Kelas:* 10
 ${htjava} *Status:* private
 
-${htjava} *Ulang Tahun:* 4 sept
-${htjava} *Alamat:* private
+${htjava} *Ulang Tahun:* 20 Juni
+${htjava} *Alamat:* Kuala,Malaysia
 ${htjava} *Skills:* 
 > JavaScript [89.7%]
 > Python [13.4%]
